781a9d0008f89ca00865b0104a219adf7e5e51ee
AnnaKybrak <147615526+AnnaKybrak@users.noreply.github.com>
2026-06-02 18:06:54 +0300

    S11 - Кубрак

--- S11/models.py
+++ S11/models.py
@@ -24,7 +24,7 @@ class Discipline(BaseModel):
             new_disc = cls.create(name=name, code=code, is_active=is_active)
             return {"id": new_disc.id, "name": new_disc.name, "code": new_disc.code}
         except IntegrityError:
-            raise ValueError("Ошибка БД: нарушение уникальности")
+            raise ValueError("error_unique_constraint")
 
     @classmethod
     def update_by_id(
@@ -38,16 +38,6 @@ class Discipline(BaseModel):
         if not discipline:
             return None
 
-        name_changed = name is not None and name != discipline.name
-        code_changed = code is not None and code != discipline.code
-
-        if name_changed or code_changed:
-            new_name = name if name is not None else discipline.name
-            new_code = code if code is not None else discipline.code
-            existing = cls.get_or_none((cls.name == new_name) & (cls.code == new_code))
-            if existing and existing.id != discipline.id:
-                raise ValueError("Комбинация name и code должна быть уникальной")
-
         if name is not None:
             discipline.name = name
         if code is not None:
@@ -59,16 +49,20 @@ class Discipline(BaseModel):
             discipline.save()
             return {"id": discipline.id, "status": "updated"}
         except IntegrityError:
-            raise ValueError("Ошибка БД при обновлении: нарушение уникальности")
+            raise ValueError("error_unique_constraint")
 
     @classmethod
     def delete_discipline_by_id(cls, discipline_id: int):
         discipline = cls.get_or_none(cls.id == discipline_id)
         if not discipline:
             return False
-        discipline.is_active = False
-        discipline.save()
-        return True
+
+        if discipline.is_active:
+            discipline.is_active = False
+            discipline.save()
+            return True
+
+        return False
 
     @classmethod
     def get_discipline_by_id(cls, discipline_id: int):
@@ -83,14 +77,16 @@ class Discipline(BaseModel):
         }
 
     @classmethod
-    def get_disciplines_list(cls, name: str = None, is_active: bool = None):
+    def get_disciplines_list(
+        cls, name: str = None, is_active: bool = None, limit: int = 100, offset: int = 0
+    ):
         query = cls.select()
         if name:
-            query = query.where(cls.name == name)
+            query = query.where(cls.name.contains(name))
         if is_active is not None:
             query = query.where(cls.is_active == is_active)
 
-        query = query.order_by(cls.id)
+        query = query.order_by(cls.id).limit(limit).offset(offset)
 
         return [
             {"id": d.id, "name": d.name, "code": d.code, "is_active": d.is_active}
