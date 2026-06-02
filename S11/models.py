7f944f6a6e9257c2722ff743877c579416721599
AnnaKybrak <147615526+AnnaKybrak@users.noreply.github.com>
2026-06-02 17:51:47 +0300

    S11 - Кубрак

--- S11/models.py
+++ S11/models.py
@@ -20,10 +20,6 @@ class Discipline(BaseModel):
 
     @classmethod
     def add_discipline(cls, name: str, code: str, is_active: bool = True):
-        existing = cls.get_or_none((cls.name == name) & (cls.code == code))
-        if existing:
-            raise ValueError("Дисциплина с таким именем и кодом уже существует")
-
         try:
             new_disc = cls.create(name=name, code=code, is_active=is_active)
             return {"id": new_disc.id, "name": new_disc.name, "code": new_disc.code}
@@ -42,12 +38,12 @@ class Discipline(BaseModel):
         if not discipline:
             return None
 
-        new_name = name if name is not None else discipline.name
-        new_code = code if code is not None else discipline.code
+        name_changed = name is not None and name != discipline.name
+        code_changed = code is not None and code != discipline.code
 
-        if (name is not None and name != discipline.name) or (
-            code is not None and code != discipline.code
-        ):
+        if name_changed or code_changed:
+            new_name = name if name is not None else discipline.name
+            new_code = code if code is not None else discipline.code
             existing = cls.get_or_none((cls.name == new_name) & (cls.code == new_code))
             if existing and existing.id != discipline.id:
                 raise ValueError("Комбинация name и code должна быть уникальной")
@@ -68,13 +64,11 @@ class Discipline(BaseModel):
     @classmethod
     def delete_discipline_by_id(cls, discipline_id: int):
         discipline = cls.get_or_none(cls.id == discipline_id)
-        if discipline is None:
+        if not discipline:
             return False
-        if discipline.is_active:
-            discipline.is_active = False
-            discipline.save()
-            return True
-        return False
+        discipline.is_active = False
+        discipline.save()
+        return True
 
     @classmethod
     def get_discipline_by_id(cls, discipline_id: int):
@@ -92,7 +86,7 @@ class Discipline(BaseModel):
     def get_disciplines_list(cls, name: str = None, is_active: bool = None):
         query = cls.select()
         if name:
-            query = query.where(cls.name.contains(name))
+            query = query.where(cls.name == name)
         if is_active is not None:
             query = query.where(cls.is_active == is_active)
 
